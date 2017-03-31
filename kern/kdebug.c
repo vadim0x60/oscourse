872dfcce224d3c58b3ab0b66d3794dae283cef22
Vadim Liventsev <liventsevvadim@gmail.com>
2017-03-31 17:42:19 +0300

    Kdebug

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -143,6 +143,8 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 		// Return -1 if it is not.  Hint: Call user_mem_check.
 		// LAB 8: Your code here.
 
+		if (user_mem_check(curenv, usd, sizeof(struct UserStabData), 0)) return -1;
+
 		stabs = usd->stabs;
 		stab_end = usd->stab_end;
 		stabstr = usd->stabstr;
@@ -150,6 +152,11 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 
 		// Make sure the STABS and string table memory is valid.
 		// LAB 8: Your code here.
+
+		if (user_mem_check(curenv, stabs, (int)(stab_end - stabs), 0) ||
+			user_mem_check(curenv, stabs, (int)(stabstr_end - stabstr), 0)) {
+				return -1;
+			}
 	}
 
 	// String table validity checks
